8a3d8ce997bd297acb555304fb636524fd8598dc
ruochen1112 <q.ruochen@gmail.com>
2017-10-03 02:09:31 -0500

    Merging on another data source

--- Assignment2.R
+++ Assignment2.R
@@ -1,6 +1,8 @@
 #Ruochen Qiu
 #This file is an exploratory analysis of the DoT FARS data.
 
+
+#############Load the FARS Data into R###################
 #Put them here just to show the process been did for the assignment
 #install.packages("readr")
 #install.packages("haven")
@@ -22,4 +24,29 @@ acc2015 <- read_csv("accident.csv")
 ls()
 class(acc2014)
 class(acc2015)
+
+############Combining the two years of FARS data############
+
+acc2014 <- acc2014 %>% as_tibble() %>% mutate(
+  TWAY_ID2 = na_if(TWAY_ID2, "")
+)
+
+table(is.na(acc2014$TWAY_ID2))
+
+dim(acc2014)
+dim(acc2015)
+
+colnames(acc2014) %in% colnames(acc2015)
+colnames(acc2015) %in% colnames(acc2014)
+#"RUR_URB", "FUNC_SYS","RD_OWNER" are not in acc2014 
+#"ROAD_FNC" are not in acc 2015
+
+acc <- bind_rows(acc2014, acc2015)
+count(acc, RUR_URB)
+# The bind_rows function simply bind two tibbles togther without adding anything.
+# The reason of get 30056 NA is that no column named RUR_URB exists in "acc2014", when combine these two tibbles, 
+# the rows which were belong to acc2014 are showing as NA in new tibble "acc" since there is no such data of RUR_URB in 2014.
+
+
+############Merging on another data source####################
 

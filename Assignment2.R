48fb077971f7e4ecf930cfe391797bb3a8f57215
ruochen1112 <q.ruochen@gmail.com>
2017-10-03 00:58:03 -0500

    Load the FARS Data into R

--- Assignment2.R
+++ Assignment2.R
@@ -1,1 +1,25 @@
-test
+#Ruochen Qiu
+#This file is an exploratory analysis of the DoT FARS data.
+
+#Put them here just to show the process been did for the assignment
+#install.packages("readr")
+#install.packages("haven")
+#install.packages("dplyr")
+#install.packages("tidyr")
+#install.packages("stringr")
+#install.packages("ggplot2")
+
+library(readr)
+library(haven)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(ggplot2)
+
+acc2014 <- read_sas("accident.sas7bdat")
+acc2015 <- read_csv("accident.csv")
+
+ls()
+class(acc2014)
+class(acc2015)
+
